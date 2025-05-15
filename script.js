747ed6a7ed15a37c1287fd0fee1ad6bcea03cf72
Amarnath Reddy <tamarnathr@gmail.com>
2025-05-15 07:47:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+    // Trim leading spaces
+    str = str.trimStart();
+
+    // Find the index of the first space
+    const spaceIndex = str.indexOf(' ');
+
+    // If no space is found, return the entire string
+    if (spaceIndex === -1) {
+        return str;
+    }
+
+    // Return the substring from start to the first space
+    return str.substring(0, spaceIndex);
 }
 
-// Do not change the code below
-
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// Example usage:
+console.log(firstWord('see and stop'));   // Output: 'see'
+console.log(firstWord(' Hello World!'));  // Output: 'Hello'
+console.log(firstWord('12345'));          // Output: '12345'
+console.log(firstWord(''));               // Output: ''
